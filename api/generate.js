c36e9ee710018bf586df6510d0de77f25cae23bf
XanderDevelops <88814234+XanderDevelops@users.noreply.github.com>
2025-08-24 15:18:54 -0400

    fixes

--- api/generate.js
+++ api/generate.js
@@ -10,6 +10,14 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 // The main function Vercel will run
 export default async function handler(req, res) {
+
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+        return res.status(200).end();
+    }
+
     // We only allow POST requests to this endpoint
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
